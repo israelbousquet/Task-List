ee84c1ba9f84c0c8d1c4f5672e3f432f1b47c62f
israelbousquet <israelbousquet@gmail.com>
2023-03-27 09:36:22 -0300

    att

--- src/app/animations/fade.animation.ts
+++
@@ -1,9 +0,0 @@
-import { trigger, style, transition, animate } from '@angular/animations';
-
-export const fade = trigger('fade', [
-  transition(':enter', [
-    style({ opacity: 0 }),
-    animate('0.2s', style({ opacity: 1 })),
-  ]),
-  transition(':leave', [animate('0.2s', style({ opacity: 0 }))]),
-]);
