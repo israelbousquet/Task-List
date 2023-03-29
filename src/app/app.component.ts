0b6888e6e11365a47a0bdf7353356f0c812f7fe0
israelbousquet <israelbousquet@gmail.com>
2023-03-29 12:41:11 -0300

    adicionado modo noturno e toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  isChecked: boolean = false;
+  nameMode: string = 'nightlight_round';
   title = 'task-list';
+
+  ngOnInit() {}
+
+  changed(event: MatSlideToggleChange) {
+    this.nameMode = event.checked ? 'light_mode' : 'nightlight_round';
+    document.body.classList.toggle('ligthMode');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserModule } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+} from '@angular/platform-browser/animations';
 import { HotToastModule } from '@ngneat/hot-toast';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxMaskModule } from 'ngx-mask';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -34,6 +38,8 @@ import { EditDialogComponent } from './widgets/edit-dialog/edit-dialog.component
     HttpClientModule,
     BrowserAnimationsModule,
     NgxMaskModule.forRoot(),
+    MatSlideToggleModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
